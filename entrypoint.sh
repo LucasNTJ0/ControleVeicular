662aaa01b9ffd6df713b705557e6e21f938c893c
Lucas Vinícius DEV <lucasvinicius3431@gmail.com>
2025-09-11 15:27:55 -0300

    Alpha Test 0.0.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
 
-echo "Ajustando permissões de storage e cache..."
+echo "Ajustando permissões de storage e bootstrap/cache..."
+
+# Cria pastas caso não existam
+mkdir -p /var/www/html/storage /var/www/html/bootstrap/cache
+
+# Ajusta proprietário e permissões
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
